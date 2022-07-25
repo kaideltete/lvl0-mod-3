f2f7145f8e02f912da308695945356fb26a34f78
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-25 16:33:41 -0700

    Update fizz_buzz.py

--- _03_modulo/_4_fizz_buzz/fizz_buzz.py
+++ _03_modulo/_4_fizz_buzz/fizz_buzz.py
@@ -13,3 +13,5 @@
    -
    1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
 """
+if __name__ == '__main__':
+    print("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz")
